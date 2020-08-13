9bb9c44739884dbf53a0ef9bbcc9e352682315ac
Damian Duszyński <dd.duszynski@gmail.com>
2020-08-13 20:43:16 +0200

    lesson 154. Loading Places

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -6,15 +6,14 @@ import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
 import './Auth.css';
 
 const Auth = () => {
 	const auth = useContext(AuthContext);
 	const [isLoginMode, setIsLoginMode] = useState(true);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState();
-
+	const { isLoading, error, sendRequest, clearError } = useHttpClient();
 	const [formState, inputHandler, setFormData] = useForm(
 		{
 			email: {
@@ -55,65 +54,49 @@ const Auth = () => {
 
 	const authSubmitHandler = async (event) => {
 		event.preventDefault();
-		setIsLoading(true);
 
 		if (isLoginMode) {
 			try {
-				const response = await fetch('http://localhost:5000/api/users/login', {
-               method: 'POST',
-               //jeżeli nie wysyłam danych, nie muszę definiować Content-Type
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify({
+				const responseData = await sendRequest(
+					'http://localhost:5000/api/users/login',
+					'POST',
+					JSON.stringify({
 						email: formState.inputs.email.value,
 						password: formState.inputs.password.value,
 					}),
-				});
-				const responseData = await response.json();
-				//tu sprawdzam czy status będzie 400 lub 500
-				if (!response.ok) {
-					throw new Error(responseData.message);
-				}
-				setIsLoading(false);
-				auth.login();
+					{
+						'Content-Type': 'application/json',
+					}
+				);
+				auth.login(responseData.user.id);
 			} catch (err) {
-				setIsLoading(false);
-				setError(err.message || 'Something went wrong!');
+				console.log(err);
 			}
 		} else {
 			try {
-				const response = await fetch('http://localhost:5000/api/users/signup', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify({
+				const responseData = await sendRequest(
+					'http://localhost:5000/api/users/signup',
+					'POST',
+					JSON.stringify({
 						name: formState.inputs.name.value,
 						email: formState.inputs.email.value,
 						password: formState.inputs.password.value,
 					}),
-				});
-				const responseData = await response.json();
-				//tu sprawdzam czy status będzie 400 lub 500
-				if (!response.ok) {
-					throw new Error(responseData.message);
-				}
-				setIsLoading(false);
-				auth.login();
+					{
+						'Content-Type': 'application/json',
+					}
+				);
+
+				auth.login(responseData.user.id);
 			} catch (err) {
-				setIsLoading(false);
-				setError(err.message || 'Something went wrong!');
+				console.log(err);
 			}
 		}
-	};
-	const errorHandler = () => {
-		setError(null);
 	};
 
 	return (
 		<>
-			<ErrorModal error={error} onClear={errorHandler} />
+			<ErrorModal error={error} onClear={clearError} />
 			<Card className='authentication'>
 				{isLoading && <LoadingSpinner asOverlay />}
 				<h2>Login Required</h2>
