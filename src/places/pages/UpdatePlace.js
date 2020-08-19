3bff6d8d68bf30919ee03dd838785b00fdc645cf
Damian Duszyński <dd.duszynski@gmail.com>
2020-08-19 09:28:18 +0200

    lesson 202 - deploying

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -34,7 +34,7 @@ const UpdatePlace = () => {
 	useEffect(() => {
 		const fetchPlace = async () => {
 			try {
-				const responseData = await sendRequest(`http://localhost:5000/api/places/${placeId}`);
+				const responseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`);
 				setLoadedPlace(responseData.place);
 				setFormData(
 					{
@@ -60,7 +60,7 @@ const UpdatePlace = () => {
 		event.preventDefault();
 		try {
 			sendRequest(
-				`http://localhost:5000/api/places/${placeId}`,
+				`${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`,
 				'PATCH',
 				JSON.stringify({
 					title: formState.inputs.title.value,
